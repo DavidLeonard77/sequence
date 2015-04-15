ce79f9063a92cfeb1e0d397cf5e14bd428b1b201
David Leonard <dleonard77@me.com>
2015-04-14 20:55:51 -0700

    Final Commit

---
+++ app/scripts/app.js
@@ -0,0 +1,46 @@
+'use strict';
+
+
+/**
+ * @ngdoc overview
+ * @name mbcApp
+ * @description
+ * # mbcApp
+ *
+ * Main module of the application.
+ */
+
+angular
+  .module('mbcApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .controller("storeCtrl", function($scope, $http) {
+
+      $scope.storeData = {};
+
+      $.get('http://seq-front-end-assessment.s3-website-us-west-2.amazonaws.com/catalog.json', function (data) {
+
+        $scope.storeData = data;
+        $scope.$apply();
+
+        $scope.$on('postCarouselDirective', function(scope, element, attrs){    
+
+          // Load cover social icons
+          $('.carousel-inner > div:first-child').addClass('active');
+          $('#myCarousel li:first-child').addClass('active');
+
+        });
+
+      });
+
+  })
+  .directive('postCarouselDirective',function(){
+    return function(scope, element, attrs) {
+      if (scope.$last) setTimeout(function(){ scope.$emit('postCarouselDirective', element, attrs) }, 1);
+    };
+  });
